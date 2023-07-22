f14f2ab6415fc696f47ad9279f2b1d6db3629f2d
guitjav <139990005+guitjav@users.noreply.github.com>
2023-07-22 14:53:47 -0300

    Update streamlit_app.py
    1-addEmojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,11 @@ import streamlit
 streamlit.title('hey hey hey')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑 Avocado Toast')
+
+
 
 
 
