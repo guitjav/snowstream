efc8f6a3551fbddf466211df1c9baf04bf97abd9
guitjav <139990005+guitjav@users.noreply.github.com>
2023-07-22 14:51:59 -0300

    Update streamlit_app.py
    1-addHeaderAndText

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import streamlit
 
 streamlit.title('hey hey hey')
 
-streamlit.title('asi es')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
 
 
